1aab6580672d838c0ea48de1305f41ed6cb2d1d7
PatricioHML <patriciohml@hotmail.com>
2018-05-14 01:19:21 -0500

    versión final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,61 +11,242 @@ var userPower = 0;
 var wins = 0;
 var losses = 0;
 
+
+//functions
+
+//i know i am repeating my code alot, I had all the ifs on a function called endConditions() but for 
+//some reason it didnt work
+//i ll be more elegant in the future
+//as i write this i think i was calling endCondition() not endConditions()...but oh well...
+ 
+
+
+
+
 //on clicks
-$( "#yellow" ).click(function() {
-    userPower = userPower + gemY;
-    console.log(gemY);
-    console.log(userPower);
-    $( "#power" ).html("Your power level: <br>" + userPower);
+    $( "#yellow" ).click(function() {
+        userPower = userPower + gemY;
+        console.log(gemY);
+        console.log(userPower);
+        $( "#power" ).html("Your power level: <br>" + userPower);
+        if(userPower == matchPower) {
+            alert("u win");
+            wins++;
+            $("#wins").html("Wins: " + wins);
+            userPower = 0;
+            matchPower = 0;
+            matchPower = Math.floor(Math.random() * 101 + 19);
+            $( "#toGuess" ).html("Match this power level: <br>" + matchPower);
+            $( "#power" ).html("Your power level: <br>" + userPower);
+
+            gemY = 0;
+            gemO = 0;
+            gemB = 0;
+            gemP = 0;
+            gemY = Math.floor(Math.random() * 11 + 1);
+            gemO = Math.floor(Math.random() * 11 + 1);
+            gemB = Math.floor(Math.random() * 11 + 1);
+            gemP = Math.floor(Math.random() * 11 + 1);
+
+
+
+        }
+        if(userPower > matchPower) {
+            alert("u lose");
+            losses++;
+            $("#losses").html("Losses: " + losses);
+            userPower = 0;
+            matchPower = 0;
+            matchPower = Math.floor(Math.random() * 101 + 19);
+            $( "#toGuess" ).html("Match this power level: <br>" + matchPower);
+            $( "#power" ).html("Your power level: <br>" + userPower);
+
+            gemY = 0;
+            gemO = 0;
+            gemB = 0;
+            gemP = 0;
+            gemY = Math.floor(Math.random() * 11 + 1);
+            gemO = Math.floor(Math.random() * 11 + 1);
+            gemB = Math.floor(Math.random() * 11 + 1);
+            gemP = Math.floor(Math.random() * 11 + 1);
+
+
+        }
 
   });                               
 
 
- $( "#orange" ).click(function() {
+    $( "#orange" ).click(function() {
         userPower = userPower + gemO;
         console.log(gemO);
         console.log(userPower);
         $( "#power" ).html("Your power level: <br>" + userPower);
+        if(userPower == matchPower) {
+            alert("u win");
+            wins++;
+            $("#wins").html("Wins: " + wins);
+            userPower = 0;
+            matchPower = 0;
+            matchPower = Math.floor(Math.random() * 101 + 19);
+            $( "#toGuess" ).html("Match this power level: <br>" + matchPower);
+            $( "#power" ).html("Your power level: <br>" + userPower);
+
+            gemY = 0;
+            gemO = 0;
+            gemB = 0;
+            gemP = 0;
+            gemY = Math.floor(Math.random() * 11 + 1);
+            gemO = Math.floor(Math.random() * 11 + 1);
+            gemB = Math.floor(Math.random() * 11 + 1);
+            gemP = Math.floor(Math.random() * 11 + 1);
+
+
+
+        }
+        if(userPower > matchPower) {
+            alert("u lose");
+            losses++;
+            $("#losses").html("Losses: " + losses);
+            userPower = 0;
+            matchPower = 0;
+            matchPower = Math.floor(Math.random() * 101 + 19);
+            $( "#toGuess" ).html("Match this power level: <br>" + matchPower);
+            $( "#power" ).html("Your power level: <br>" + userPower);
+
+            gemY = 0;
+            gemO = 0;
+            gemB = 0;
+            gemP = 0;
+            gemY = Math.floor(Math.random() * 11 + 1);
+            gemO = Math.floor(Math.random() * 11 + 1);
+            gemB = Math.floor(Math.random() * 11 + 1);
+            gemP = Math.floor(Math.random() * 11 + 1);
+
+
+
+        }
 
   });         
   
- $( "#blue" ).click(function() {
+    $( "#blue" ).click(function() {
         userPower = userPower + gemB;
         console.log(gemB);
         console.log(userPower);
         $( "#power" ).html("Your power level: <br>" + userPower);
+        if(userPower == matchPower) {
+            alert("u win");
+            wins++;
+            $("#wins").html("Wins: " + wins);
+            userPower = 0;
+            matchPower = 0;
+            matchPower = Math.floor(Math.random() * 101 + 19);
+            $( "#toGuess" ).html("Match this power level: <br>" + matchPower);
+            $( "#power" ).html("Your power level: <br>" + userPower);
+
+            gemY = 0;
+            gemO = 0;
+            gemB = 0;
+            gemP = 0;
+            gemY = Math.floor(Math.random() * 11 + 1);
+            gemO = Math.floor(Math.random() * 11 + 1);
+            gemB = Math.floor(Math.random() * 11 + 1);
+            gemP = Math.floor(Math.random() * 11 + 1);
+
+
+
+        }
+        if(userPower > matchPower) {
+            alert("u lose");
+            losses++;
+            $("#losses").html("Losses: " + losses);
+            userPower = 0;
+            matchPower = 0;
+            matchPower = Math.floor(Math.random() * 101 + 19);
+            $( "#toGuess" ).html("Match this power level: <br>" + matchPower);
+            $( "#power" ).html("Your power level: <br>" + userPower);
+
+            gemY = 0;
+            gemO = 0;
+            gemB = 0;
+            gemP = 0;
+            gemY = Math.floor(Math.random() * 11 + 1);
+            gemO = Math.floor(Math.random() * 11 + 1);
+            gemB = Math.floor(Math.random() * 11 + 1);
+            gemP = Math.floor(Math.random() * 11 + 1);
+
+
+
+        }
+        
 
   }); 
 
-  $( "#purple" ).click(function() {
-    userPower = userPower + gemP;
-    console.log(gemP);
-    console.log(userPower);
-    $( "#power" ).html("Your power level: <br>" + userPower);
+     $( "#purple" ).click(function() {
+        userPower = userPower + gemP;
+        console.log(gemP);
+        console.log(userPower);
+        $( "#power" ).html("Your power level: <br>" + userPower);
+        if(userPower == matchPower) {
+            alert("u win");
+             wins++;
+            $("#wins").html("Wins: " + wins);
+            userPower = 0;
+            matchPower = 0;
+            matchPower = Math.floor(Math.random() * 101 + 19);
+            $( "#toGuess" ).html("Match this power level: <br>" + matchPower);
+            $( "#power" ).html("Your power level: <br>" + userPower);
+
+            gemY = 0;
+            gemO = 0;
+            gemB = 0;
+            gemP = 0;
+            gemY = Math.floor(Math.random() * 11 + 1);
+            gemO = Math.floor(Math.random() * 11 + 1);
+            gemB = Math.floor(Math.random() * 11 + 1);
+            gemP = Math.floor(Math.random() * 11 + 1);
+
+
+
+    }
+        if(userPower > matchPower) {
+            alert("u lose");
+            losses++;
+            $("#losses").html("Losses: " + losses);
+            userPower = 0;
+            matchPower = 0;
+            matchPower = Math.floor(Math.random() * 101 + 19);
+            $( "#toGuess" ).html("Match this power level: <br>" + matchPower);
+            $( "#power" ).html("Your power level: <br>" + userPower);
+
+            gemY = 0;
+            gemO = 0;
+            gemB = 0;
+            gemP = 0;
+            gemY = Math.floor(Math.random() * 11 + 1);
+            gemO = Math.floor(Math.random() * 11 + 1);
+            gemB = Math.floor(Math.random() * 11 + 1);
+            gemP = Math.floor(Math.random() * 11 + 1);
+
+
+
+    }
 
 }); 
 
-if (userPower == matchPower) {
-    alert("u win");
-    wins++;
-} 
-
-if (userPower > matchPower) {
-    alert("u lose");
-    losses++;
-}
-  
 
 
-
-
-
-
-
-//soul gem takeover
 $( "#toGuess" ).html("Match this power level: <br>" + matchPower);
-$( "#wins" ).html("Wins: " + wins);
-$( "#losses" ).html("Losses: " + wins);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
